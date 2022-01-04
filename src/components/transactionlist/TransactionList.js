ed3bc1538c8713bf9a7f7397861a74c238935465
sutejakw <sutejakawinanga29@gmai.com>
2022-01-05 01:04:00 +0800

    delete transaction

--- src/components/transactionlist/TransactionList.js
+++ src/components/transactionlist/TransactionList.js
@@ -1,7 +1,11 @@
+import { useFirestore } from "../../hooks/useFirestore";
+
 // styles
 import styles from "./TransactionList.module.css";
 
 export default function TransactionList({ transactions }) {
+  const { deleteDocument, response } = useFirestore("transactions");
+
   return (
     <div className={styles.transactions}>
       {transactions.map((transaction) => {
@@ -9,6 +13,7 @@ export default function TransactionList({ transactions }) {
           <li key={transaction.id}>
             <p className={styles.name}>{transaction.name}</p>
             <p className={styles.amount}>${transaction.amount}</p>
+            <button onClick={() => deleteDocument(transaction.id)}>x</button>
           </li>
         );
       })}
